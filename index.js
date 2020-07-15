844d258b8784f30970e925d5abb62ed081581aca
anmolk18 <anmolpreet.kaur@yale.edu>
2020-07-15 19:48:12 -0400

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(string => capitalize(string))
+}
+
+function capitalize(string) {
+  let stringSplit = string.split(" ")
+  return stringSplit.map(word => word[0].toUpperCase() + word.slice(1)).join(" ")
 }
