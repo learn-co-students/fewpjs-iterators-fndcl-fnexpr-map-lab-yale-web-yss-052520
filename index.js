81d86a316c9159269fb98658032df0fad1a93b96
arthur318 <arthurhwang318@gmail.com>
2020-07-16 00:12:53 -0700

    done

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  let newArray = tutorials.map(sentence => 
+    sentence.split(' ')
+    .map( word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ')
+    )
+
+
+  return newArray
 }
+
