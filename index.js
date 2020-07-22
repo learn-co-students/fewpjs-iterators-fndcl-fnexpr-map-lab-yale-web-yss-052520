df3a270526e516265829194dac14c99c5dcd3f43
Jonkavaliauskas <jonas.kavaliauskas@yale.edu>
2020-07-22 13:12:03 +0300

    Done

--- index.js
+++ index.js
@@ -12,5 +12,8 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(newline => {
+    let output = newline.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+    return output
+  })
 }
