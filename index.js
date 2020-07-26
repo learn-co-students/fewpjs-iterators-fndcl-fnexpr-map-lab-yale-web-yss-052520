20b0f93b2426e7abc22c6919eaf9a4ec88082f37
tbartelsmeyer <47281160+tbartelsmeyer@users.noreply.github.com>
2020-07-25 21:40:02 -0500

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(el => titleCase(el))
+}
+
+function titleCase(str) {
+   const splitStr = str.split(' ');
+   for (let i = 0; i < splitStr.length; i++) {
+       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+   }
+   return splitStr.join(' ');
 }
